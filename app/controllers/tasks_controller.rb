ebd878f63761ca5643f09f1fe97d9d21ed03f96d
sudo.shunsuke <s_sudo_j@yahoo.co.jp>
2019-12-23 08:28:25 +0000

    edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :edit]
   
   def create
     @task = current_user.tasks.build(task_params)
@@ -11,6 +11,22 @@ class TasksController < ApplicationController
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash[:danger] = "タスクの登録に失敗しました"
       render "toppages/index"
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクを編集しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "タスクの編集に失敗しました"
+      render :edit
     end
   end
 
